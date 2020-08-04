1b251048dcc40ad9352cdb87c119b4b67778cf12
dchicchon <danielchicchon@gmail.com>
2020-08-04 10:02:57 -0700

    add navbar, footer, places section. Brought in svg map of sf

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+
+// Style
+import "./style.css";
+
+// export default function Navbar() {
+//   return (
+
+//   );
+// }
