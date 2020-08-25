c4aec0057d6f1421ccda3bb4def51cbbf0e80a4a
dchicchon <danielchicchon@gmail.com>
2020-08-24 21:03:10 -0700

    add google analytics

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,23 +1,21 @@
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="https://www.gstatic.com/firebasejs/7.17.2/firebase-app.js"></script>
+import * as firebase from "firebase/app";
+import "firebase/database";
+import "firebase/analytics";
 
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="https://www.gstatic.com/firebasejs/7.17.2/firebase-analytics.js"></script>
+export const firebaseConfig = {
+  apiKey: "AIzaSyDZLb_DPsrPliq9sZrORiTjUKhXl0gIqyQ",
+  authDomain: "leaderboard-8c38f.firebaseapp.com",
+  databaseURL: "https://leaderboard-8c38f.firebaseio.com",
+  projectId: "leaderboard-8c38f",
+  storageBucket: "leaderboard-8c38f.appspot.com",
+  messagingSenderId: "98306186489",
+  appId: "1:98306186489:web:f7195e95ac564ad9ea9628",
+  measurementId: "G-9KL6TM54SM",
+};
 
-// <script>
-//   // Your web app's Firebase configuration
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyDZLb_DPsrPliq9sZrORiTjUKhXl0gIqyQ",
-//     authDomain: "leaderboard-8c38f.firebaseapp.com",
-//     databaseURL: "https://leaderboard-8c38f.firebaseio.com",
-//     projectId: "leaderboard-8c38f",
-//     storageBucket: "leaderboard-8c38f.appspot.com",
-//     messagingSenderId: "98306186489",
-//     appId: "1:98306186489:web:f7195e95ac564ad9ea9628",
-//     measurementId: "G-9KL6TM54SM"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
-// </script>
+firebase.initializeApp(firebaseConfig);
+
+export const analytics = firebase.analytics();
+export function analyticsClick(e) {
+  analytics.logEvent(e.currentTarget.className);
+}
